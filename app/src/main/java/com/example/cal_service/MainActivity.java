855477adbc6e1d3b0029b9bcbe6df684a4549ea1
succeessful <18212325202@163.com>
2020-10-09 22:51:33 +0800

    test version

--- app/src/main/java/com/example/cal_service/MainActivity.java
+++ app/src/main/java/com/example/cal_service/MainActivity.java
@@ -1,25 +1,92 @@
 package com.example.cal_service;
 
+import android.app.Service;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MyServiceTag";
 
+    Service_cal service_cal = null;
+    Button start,stop,add,sub,mul,div;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-    public void onClick(View v){
-        int i = v.getId();
-        switch(i){
-            case R.id.start:
-                startService(new Intent(MainActivity.this,Service_cal.class));
-            case R.id.stop:
-                stopService(new Intent(MainActivity.this,Service_cal.class));
-        }
+
+        final ServiceConnection serviceConnection = new ServiceConnection() {
+            @Override
+            public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+                Log.v(TAG, "onServiceConnect()");
+                service_cal = ((Service_cal.LocalBinder) iBinder).getService();
+            }
+
+            @Override
+            public void onServiceDisconnected(ComponentName componentName) {
+                Log.v(TAG, "onServiceDisconnected()");
+            }
+        };
+        start = findViewById(R.id.start);
+        stop = findViewById(R.id.stop);
+        add = findViewById(R.id.add);
+        sub = findViewById(R.id.sub);
+        mul = findViewById(R.id.mul);
+        div = findViewById(R.id.div);
+
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                bindService(new Intent(MainActivity.this,Service_cal.class),serviceConnection, Service.BIND_AUTO_CREATE);
+            }
+        });
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(service_cal!=null){
+                    Log.v(TAG,"add service:"+service_cal.add(1,2));
+                }
+            }
+        });
+        sub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(service_cal!=null){
+                    Log.v(TAG,"sub service:"+service_cal.sub(5,2));
+                }
+            }
+        });
+        mul.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(service_cal!=null){
+                    Log.v(TAG,"mul service:"+service_cal.mul(5,2));
+                }
+            }
+        });
+        div.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(service_cal!=null){
+                    Log.v(TAG,"div service:"+service_cal.div(6,2));
+                }
+            }
+        });
+
+        stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                unbindService(serviceConnection);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/cal_service/Service_cal.java
+++ app/src/main/java/com/example/cal_service/Service_cal.java
@@ -2,6 +2,7 @@ package com.example.cal_service;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Binder;
 import android.os.IBinder;
 import android.util.Log;
 import android.widget.EditText;
@@ -9,14 +10,23 @@ import android.widget.EditText;
 public class Service_cal extends Service {
     private static final String TAG = "MyServiceTag";
 
+    private LocalBinder myBinder = new LocalBinder();
+
+    public class LocalBinder extends Binder {
+        Service_cal getService(){
+            return Service_cal.this;
+        }
+    }
     public Service_cal() {
     }
 
     public void onCreate(){
+        Log.v(TAG,"onCreate()");
         super.onCreate();
     }
 
     public int onStartCommand(Intent intent,int flags,int startId){
+        Log.v(TAG,"onStartCommand()");
         return super.onStartCommand(intent,flags,startId);
     }
 
@@ -27,7 +37,25 @@ public class Service_cal extends Service {
 
     @Override
     public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+        Log.v(TAG,"onBind()");
+        return myBinder;
+    }
+
+    public boolean onUnBind(Intent intent) {
+        Log.v(TAG,"onUnBind()");
+        return super.onUnbind(intent);
+    }
+
+    public int add(int x,int y){
+        return x+y;
+    }
+    public int sub(int x,int y){
+        return x-y;
+    }
+    public int mul(int x,int y){
+        return x*y;
+    }
+    public float div(int x,int y){
+        return (float) x/y;
     }
 }
